605befa783beb1512a00ab5f190e5b49a6dd52aa
timokuper <timo.kuper@gmail.com>
2018-08-24 10:47:29 +0200

    Inital Version.

---
+++ M3UCreator/M3UCreator.cs
@@ -0,0 +1,48 @@
+﻿using Id3;
+using System;
+using System.IO;
+using System.Linq;
+
+namespace M3UCreator
+{
+    public class M3UCreator
+    {
+        /// <summary>
+        /// Generates a m3u file with all mp3 files in the specified directory
+        /// </summary>
+        /// <param name="mp3Directory"></param>
+        /// <param name="m3uName">The optional name of the m3u file. If not specified, the name of the directory containing the mp3 files is used.</param>
+        /// <param name="m3uDirectory">The optional directory where the m3u file is created. If not specified, the m3u file is created in the directory containing the mp3 files.</param>
+        public void CreateM3u(string mp3Directory, string m3uName = null, string m3uDirectory = null)
+        {
+            var directoryInfo = new DirectoryInfo(mp3Directory);
+
+            string m3uFilename = String.IsNullOrEmpty(m3uName) ? directoryInfo.Name : m3uName;
+            m3uFilename = Path.ChangeExtension(m3uFilename, ".m3u");
+
+            string m3uDirectoryName = String.IsNullOrEmpty(m3uDirectory) ? mp3Directory : m3uDirectory;
+            Directory.CreateDirectory(m3uDirectoryName);
+
+            string m3uFile = Path.Combine(m3uDirectoryName, m3uFilename);
+            
+
+            using (var m3u = new StreamWriter(m3uFile, false))
+            {
+                m3u.WriteLine("#EXTM3U");
+
+                foreach (string mp3File in Directory.EnumerateFiles(mp3Directory, "*.mp3", SearchOption.AllDirectories))
+                {
+                    String mp3FileRelative = Path.GetRelativePath(m3uDirectoryName, mp3File);
+
+                    using (var mp3 = new Mp3(mp3File))
+                    {
+                        Id3Tag tag = mp3.GetTag(Id3TagFamily.Version2X);
+
+                        m3u.WriteLine($"#EXTINF:{tag.Length.Value.TotalSeconds:F0},{tag.Artists.Value.FirstOrDefault() ?? ""} - {tag.Title}");
+                        m3u.WriteLine(mp3FileRelative);
+                    }
+                }
+            }
+        }
+    }
+}
--- M3UCreator/Program.cs
+++ M3UCreator/Program.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Microsoft.Extensions.CommandLineUtils;
+using System;
+using System.IO;
+using System.Reflection;
 
 namespace M3UCreator
 {
@@ -6,7 +9,56 @@ namespace M3UCreator
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var app = new CommandLineApplication();
+            app.Name = "M3UCreator";
+            app.Description = "Creates an m3u file from a directory that contains mp3 files";
+            app.HelpOption("-?|-h|--help");
+            app.VersionOption("-v|--version", () => {
+                return string.Format("Version {0}", Assembly.GetEntryAssembly().GetCustomAttribute<AssemblyInformationalVersionAttribute>().InformationalVersion);
+            });
+
+            var argMp3Directory = app.Argument("mp3Directory", "Directory of the mp3 files");
+            var optionOutputDirectory = app.Option("-o|--outputDirectory", "Optionally specify the directory, where the m3u file is generated", CommandOptionType.SingleValue);
+            var optionM3uName = app.Option("-n|--m3uName", "Optionally specify the name of the generated m3u file", CommandOptionType.SingleValue);
+
+            app.OnExecute(() =>
+            {
+                if (String.IsNullOrEmpty(argMp3Directory.Value))
+                {
+                    Console.WriteLine("The argument 'mp3Directory' is mandatory!");
+                    app.ShowHelp();
+                    return -1;
+                }
+
+                if (!Directory.Exists(argMp3Directory.Value))
+                {
+                    Console.WriteLine($"The specified directory containing the mp3 files does not exists: '{argMp3Directory.Value}'!");
+                    return -2;
+                }
+
+                Console.WriteLine("Generating m3u file...");
+                var m3uCreator = new M3UCreator();
+                m3uCreator.CreateM3u(argMp3Directory.Value, optionM3uName.Value(), optionOutputDirectory.Value());
+
+                return 0;
+            });
+
+
+            try
+            {
+                app.Execute(args);
+            }
+            catch (CommandParsingException ex)
+            {
+                // You'll always want to catch this exception, otherwise it will generate a messy and confusing error for the end user.
+                // the message will usually be something like:
+                // "Unrecognized command or argument '<invalid-command>'"
+                Console.WriteLine(ex.Message);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Unable to execute application: {0}", ex.Message);
+            }
         }
     }
 }
