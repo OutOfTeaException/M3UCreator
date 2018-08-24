3d45a7c13f2a70290b4653828651d4ce47bc92eb
timokuper <timo.kuper@gmail.com>
2018-08-24 09:09:38 +0200

    Add project files.

---
+++ M3UCreator/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace M3UCreator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
